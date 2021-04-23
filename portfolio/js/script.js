2f08d3ba5f0c387a2ef05353b99c03d0158eb7f1
karthikbs27 <karthik2000tbt@gmail.com>
2021-04-24 00:16:39 +0530

    commit 2

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,27 +1,33 @@
 console.log("shish")
+bodytext = "I am a front-end web designer and an asparent for learning back-end. I have great interest on web develpment, image processign and data science using machine learning ."
 
-headtext = "HI, I AM KARTHIK"
-bodytext = "shish is my new word to say in the public "
-var textid = "head"
-var bodyid = "heada"
-for(i = 0; i < 11; i++){
-    is = String(i)
-    temp = textid + is;
-    temp1 = bodyid + is;
-    document.getElementById(temp).innerHTML = headtext;
-    document.getElementById(temp1).innerHTML = bodytext;
+var i = 0 
+type()
+function type() {
+    if (i < bodytext.length) {
+      document.getElementById("body").innerHTML += bodytext.charAt(i);
+      i++;
+      setTimeout(type, 30);
+    }
 }
 
+cursor = document.getElementById("cursor")
+cursorback = document.getElementById("cursorback")
+cursorchild = document.getElementById("cursorchild")
+document.addEventListener('mousemove', e =>{
+    cursor.style.top = e.pageY + "px"
+    cursorback.style.top = e.pageY + "px"
+    cursorchild.style.top = e.pageY + "px"
+    cursor.style.left = e.pageX + "px"
+    cursorback.style.left = e.pageX + "px"
+    cursorchild.style.left = e.pageX + "px"
+})
 
-
-shadow();
-function shadow(wid = 1){
-
-    
-
-    // for(i = 0; i < 11; i++){
-    //     temp = textid + i;
-    //     document.getElementById(temp).style.left = (i * wid) + 'px';
-    // }
-
+function preload(){
+    document.getElementById("loading").style.opacity = "0"
+    var main = document.getElementById("main")
+    main.style.display = "block"
+    main.style.opacity = "1"
+  
 }
+
